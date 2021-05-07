db58d08d1bf918b1c2d9c396abc58507a02356fb
tpetillot <th.petillot@gmail.com>
2021-05-07 04:49:38 -0400

    Clean

--- assets/js/index.ts
+++ assets/js/index.ts
@@ -4,7 +4,7 @@ const suggestions = document.getElementById('suggestions');
 const userinput = document.getElementById('userinput');
 
 if (suggestions !== null && userinput !== null) {
-  initializeSearch(suggestions, userinput as HTMLInputElement)
+  initializeSearch(suggestions, userinput as HTMLInputElement);
 }
 
 type FrontMatter = {
@@ -45,13 +45,11 @@ const indexOptions: CreateOptions = {
     store: [
       'title',
       'description',
-      'href',
-      'version',
-      'latest',
-      'section',
+      'href'
     ]
   },
 }
+
 const index = FlexSearch.create<FrontMatter>(indexOptions);
 
 
@@ -61,18 +59,18 @@ const createSearchEntryElement = (frontMatter: FrontMatter): HTMLDivElement => {
   const hyperlink = document.createElement('a');
   hyperlink.href = frontMatter.href;
 
-  const title = document.createElement('span')
-  const titleText = document.createTextNode(frontMatter.title)
-  title.append(titleText)
+  const title = document.createElement('span');
+  const titleText = document.createTextNode(frontMatter.title);
+  title.append(titleText);
 
-  const description = document.createElement('span')
-  const descriptionText = document.createTextNode(frontMatter.description)
-  description.append(descriptionText)
+  const description = document.createElement('span');
+  const descriptionText = document.createTextNode(frontMatter.description);
+  description.append(descriptionText);
 
-  hyperlink.append(title)
-  hyperlink.append(description)
+  hyperlink.append(title);
+  hyperlink.append(description);
 
-  entry.append(hyperlink)
+  entry.append(hyperlink);
   return entry;
 }
 
@@ -86,19 +84,24 @@ let searchConfiguration: SearchConfiguration = {
   section: "",
   version: undefined,
   latest: undefined
-}
+};
+
 const updateSearchOptions = (section: string, version?: string, latest?: boolean): void => {
   searchConfiguration = {
     section,
     version,
     latest: `${latest === undefined ? version === undefined : latest}` // Filter on latest when version isn't set
   }
-}
+};
 
-const whileResultsInferiorSearchLimit = (fallback: (limit: number) => Promise<FrontMatter[]>) => (results: FrontMatter[]): Promise<FrontMatter[]> =>
-  results.length < searchLimit ?
-    fallback(searchLimit - results.length).then(newResults => [...results, ...newResults.slice(0, searchLimit - results.length)]) :
-    Promise.resolve(results);
+const whileResultsInferiorSearchLimit = (fallback: (limit: number) => Promise<FrontMatter[]>) => (results: FrontMatter[]): Promise<FrontMatter[]> => {
+  if (results.length < searchLimit) {
+    const nextLimit = searchLimit - results.length;
+    return fallback(nextLimit).then(newResults => [...results, ...newResults.slice(0, nextLimit)]);
+  } else {
+    return Promise.resolve(results);
+  }
+}
 
 type SearchQuery = SearchOptions & {query: string};
 const searchOptionsQueryFields = (query: string,): SearchQuery[] =>
@@ -110,7 +113,6 @@ const searchOptionsQueryFields = (query: string,): SearchQuery[] =>
 
 const search = (query: string): Promise<FrontMatter[]> => {
   if (searchConfiguration.version) {
-    console.log(searchConfiguration)
     return index.search({
       query,
       limit: searchLimit,
@@ -120,7 +122,7 @@ const search = (query: string): Promise<FrontMatter[]> => {
       }
     }).then(whileResultsInferiorSearchLimit((limit) =>
       index.search({
-        query: "only provides live feedback",
+        query,
         limit,
         where: {
           version: "null"
@@ -141,16 +143,24 @@ const search = (query: string): Promise<FrontMatter[]> => {
           bool: "not"
         }
       ])
-    ))
+    ));
   } else {
     return index.search({
       query,
+      limit: searchLimit,
       where: {
         latest: "true"
       }
     });
   }
 }
+
+const displayNoneClass = 'd-none';
+const hideElement = (element: HTMLElement, hide: boolean): void => {
+  if (hide) element.classList.add(displayNoneClass);
+  else element.classList.remove(displayNoneClass);
+}
+
 
 function initializeSearch(suggestions: HTMLElement, userinput: HTMLInputElement): void {
 
@@ -166,7 +176,7 @@ function initializeSearch(suggestions: HTMLElement, userinput: HTMLInputElement)
       // Close search
       case KeyCodes.escape:
         userinput.blur();
-        suggestions.classList.add('d-none');
+        hideElement(suggestions, true);
         break;
 
       // Navigate search
@@ -174,10 +184,10 @@ function initializeSearch(suggestions: HTMLElement, userinput: HTMLInputElement)
       case KeyCodes.down:
         e.preventDefault();
         const focusableSuggestions = suggestions.querySelectorAll('a') || [];
-        const index: number = Array.from(focusableSuggestions).indexOf(document.activeElement as HTMLAnchorElement)
+        const index: number = Array.from(focusableSuggestions).indexOf(document.activeElement as HTMLAnchorElement);
 
         const offset = e.keyCode === KeyCodes.up ? -1 : 1;
-        const nextIndex = Math.max(Math.min(index + offset, focusableSuggestions.length), 0)
+        const nextIndex = Math.max(Math.min(index + offset, focusableSuggestions.length), 0);
         focusableSuggestions[nextIndex].focus();
         break;
     }
@@ -186,7 +196,7 @@ function initializeSearch(suggestions: HTMLElement, userinput: HTMLInputElement)
   // Hide when click outside
   document.addEventListener('click', (event) => {
     if (suggestions.contains(event.target as Node)) {
-      suggestions.classList.add('d-none');
+      hideElement(suggestions, true);
     }
   });
 
@@ -198,12 +208,12 @@ function initializeSearch(suggestions: HTMLElement, userinput: HTMLInputElement)
     const results = search(value);
 
     suggestions.classList.remove('d-none');
-    const previousChildsLength = suggestions.childNodes.length
+    const previousChildsLength = suggestions.childNodes.length;
 
     results.then((frontMatters) => {
       suggestions.append(...frontMatters.map(createSearchEntryElement));
       for (let i = 0; i < previousChildsLength; i++) {
-        suggestions.firstChild && suggestions.removeChild(suggestions.firstChild)
+        suggestions.firstChild && suggestions.removeChild(suggestions.firstChild);
       }
     })
   }, true);
@@ -214,13 +224,15 @@ function initializeSearch(suggestions: HTMLElement, userinput: HTMLInputElement)
 
 fetch("/search/index.json")
   .then(response => response.json())
-  .then(search => {
-      if (search.exported) index.import(search.indexes)
-      else index.add(search.indexes)
+  .then(({ exported, indexes }) => {
+      if (exported) { // @ts-ignore
+        index.import(indexes, { serialize: false });
+      } else {
+        index.add(indexes);
+      }
   })
 
 // FIXME: js.Build import all dependencies inside concatenated file, in a self-called function preventing the export
-window.updateSearchVersion = (section: string, version?: string, latest?: boolean): void => {
+window.updateSearchVersion = (section: string, version?: string, latest?: boolean): void =>
   updateSearchOptions(section, version, latest)
-}
 
