8dba2df751abc2069a7a8dd2e5c2e15a25dcf58b
Guillaume Corré <gui.corre@gmail.com>
2021-06-01 12:59:57 +0200

    Open tab when ctrl+clicking on an image

--- assets/js/lightbox.ts
+++ assets/js/lightbox.ts
@@ -3,21 +3,24 @@
  * Copyright 2013 Jason Butz
  * http://www.apache.org/licenses/LICENSE-2.0.txt
  */
-!(function ($) {
-  $("body").on("click", ".lightbox img", function () {
-    var $body = $("body");
-    var $imgHref = $(this).attr("data-src");
-    var $lightbox = $('<div id="lightbox">');
-    var $lightboxImage = $("<img>").attr("src", $imgHref);
-    $lightbox.append($lightboxImage);
-    $lightbox.fadeIn(200);
-    $body.append($lightbox);
-    $("#lightbox").on("click", function (remove) {
-      if (remove.target == this) {
-        $lightbox.fadeOut(200, function () {
-          $("#lightbox").remove();
-        });
-      }
-    });
+(function ($) {
+  $("body").on("click", ".lightbox img", function (event) {
+    if (!event.ctrlKey) {
+      event.preventDefault();
+      var $body = $("body");
+      var $imgHref = $(this).attr("data-src");
+      var $lightbox = $('<div id="lightbox">');
+      var $lightboxImage = $("<img>").attr("src", $imgHref);
+      $lightbox.append($lightboxImage);
+      $lightbox.fadeIn(200);
+      $body.append($lightbox);
+      $("#lightbox").on("click", function (remove) {
+        if (remove.target == this) {
+          $lightbox.fadeOut(200, function () {
+            $("#lightbox").remove();
+          });
+        }
+      });
+    }
   });
 })(window.jQuery);
