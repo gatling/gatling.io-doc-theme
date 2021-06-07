3693064d073a8d86af19cf0f983ffc85e5f7167b
Guillaume Corré <gui.corre@gmail.com>
2021-06-07 22:07:19 +0200

    Remove purgecss until we can make it work along hugo modules

--- config/postcss.config.js
+++ config/postcss.config.js
@@ -3,28 +3,5 @@ const purgecss = require("@fullhuman/postcss-purgecss");
 const whitelister = require("purgecss-whitelister");
 
 module.exports = {
-  plugins: [
-    autoprefixer(),
-    purgecss({
-      content: ["./layouts/**/*.html", "./content/**/*.md"],
-      safelist: [
-        /^fa-[a-z0-9-]+$/,
-        "lazyloaded",
-        "table",
-        "thead",
-        "tbody",
-        "tr",
-        "th",
-        "td",
-        ...whitelister([
-          "./assets/scss/components/_alerts.scss",
-          "./assets/scss/components/_code.scss",
-          "./assets/scss/components/_products.scss",
-          "./assets/scss/components/_search.scss",
-          "./assets/scss/components/_syntax.scss",
-          "./assets/scss/common/_dark.scss",
-        ]),
-      ],
-    }),
-  ],
+  plugins: [autoprefixer()],
 };
