b92d1d6f9143fca3ad86a471cf671e0bb292fc13
Guillaume Corré <gcorre@gatling.io>
2021-10-26 11:19:05 +0200

    Wrap orphan pre blocks inside a div.highlight to harmozize styling, close MISC-274

--- assets/js/clipboard.ts
+++ assets/js/clipboard.ts
@@ -1,31 +1,45 @@
-import Clipboard from 'clipboard';
+import Clipboard from "clipboard";
 
-var $codes = document.querySelectorAll('.highlight');
+var $blocks = document.querySelectorAll("pre");
+
+// Hugo only wraps highlighted code blocks in a div
+// We catch orphan pre blocks and wrap them ourselves to harmonize styling
+for (let i = 0, len = $blocks.length; i < len; i++) {
+  const pre = $blocks[i];
+  if (pre.parentNode && pre.parentNode.className !== "highlight") {
+    const wrap = document.createElement("div");
+    wrap.classList.add("highlight");
+    pre.parentNode.insertBefore(wrap, pre);
+    wrap.appendChild(pre);
+  }
+}
+
+var $highlights = document.querySelectorAll(".highlight");
 
 function copyButton() {
-    var copy = document.createElement("button");
-    copy.className = "btn-clipboard btn btn-sm btn-link";
-    return copy;
+  var copy = document.createElement("button");
+  copy.className = "btn-clipboard btn btn-sm btn-link";
+  return copy;
 }
 
 function statusButton(parent) {
-    var status = document.createElement("span");
-    status.className = "copy-status";
-    parent.append(status);
+  var status = document.createElement("span");
+  status.className = "copy-status";
+  parent.append(status);
 }
 
 function clipboardButton(element) {
-    var copy = copyButton();
-    statusButton(copy);
-    element.prepend(copy);
+  var copy = copyButton();
+  statusButton(copy);
+  element.prepend(copy);
 }
 
-for (var i = 0, len = $codes.length; i < len; i++) {
-    clipboardButton($codes[i]);
+for (let i = 0, len = $highlights.length; i < len; i++) {
+  clipboardButton($highlights[i]);
 }
 
-var clipboard = new Clipboard('.btn-clipboard', {
-    target: function(trigger) {
-        return trigger.nextElementSibling;
-    }
+const clipboard = new Clipboard(".btn-clipboard", {
+  target: function (trigger) {
+    return trigger.nextElementSibling;
+  },
 });
