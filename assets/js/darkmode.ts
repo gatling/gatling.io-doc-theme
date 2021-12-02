fc2c52ae4a3fa1e476904c2b1f4aac8078e4e703
Guillaume Corré <gcorre@gatling.io>
2021-12-02 20:44:56 +0100

    Formatting

--- assets/js/darkmode.ts
+++ assets/js/darkmode.ts
@@ -1,12 +1,11 @@
-document.getElementById('mode').addEventListener('click', () => {
-
-  document.body.classList.toggle('dark');
-  localStorage.setItem('theme', document.body.classList.contains('dark') ? 'dark' : 'light');
-
+document.getElementById("mode").addEventListener("click", () => {
+  document.body.classList.toggle("dark");
+  localStorage.setItem(
+    "theme",
+    document.body.classList.contains("dark") ? "dark" : "light"
+  );
 });
 
-if (localStorage.getItem('theme') === 'dark') {
-
-  document.body.classList.add('dark');
-
+if (localStorage.getItem("theme") === "dark") {
+  document.body.classList.add("dark");
 }
