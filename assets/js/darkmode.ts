27e80af4e06ad7dc7b54067be4973ca4978bebbc
Guillaume Corré <gcorre@gatling.io>
2022-07-11 19:07:10 +0200

    fix: lift dark mode class as an attribute to html tag so it doesn't flash on reload

--- assets/js/darkmode.ts
+++ assets/js/darkmode.ts
@@ -1,11 +1,20 @@
-document.getElementById("mode").addEventListener("click", () => {
-  document.body.classList.toggle("dark");
-  localStorage.setItem(
-    "theme",
-    document.body.classList.contains("dark") ? "dark" : "light"
-  );
-});
-
-if (localStorage.getItem("theme") === "dark") {
-  document.body.classList.add("dark");
+const mode = document.getElementById('mode');
+if (mode !== null) {
+  window.matchMedia('(prefers-color-scheme: dark)').addEventListener('change', event => {
+    if (event.matches) {
+      document.documentElement.setAttribute('data-dark-mode', '');
+    } else {
+      document.documentElement.removeAttribute('data-dark-mode');
+    }
+    document.body.classList.toggle("dark");
+  })
+  mode.addEventListener('click', () => {
+    document.documentElement.toggleAttribute('data-dark-mode');
+    localStorage.setItem('theme', document.documentElement.hasAttribute('data-dark-mode') ? 'dark' : 'light');
+  });
+  if (localStorage.getItem('theme') === 'dark') {
+    document.documentElement.setAttribute('data-dark-mode', '');
+  } else {
+    document.documentElement.removeAttribute('data-dark-mode');
+  }
 }
