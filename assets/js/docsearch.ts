c35575d5c6b022bc9a4621fb062d20e71a5ea988
Guillaume Corré <gui.corre@gmail.com>
2024-06-12 16:06:05 +0200

    feat: add debounce on doc search

--- assets/js/docsearch.ts
+++ assets/js/docsearch.ts
@@ -1,13 +1,34 @@
 {{ with .Site.Params.docSearch }}
 import docsearch from "@docsearch/js";
 
+// https://docsearch.algolia.com/docs/api/#transformsearchclient
+function debounce(func, wait) {
+  let lastTimeout = null;
+  return function (...args) {
+    const that = this;
+    return new Promise((resolve, reject) => {
+      if (lastTimeout) {
+        clearTimeout(lastTimeout);
+      }
+      lastTimeout = setTimeout(() => {
+        lastTimeout = null;
+        Promise.resolve(func.apply(that, args)).then(resolve).catch(reject);
+      }, wait);
+    });
+  };
+}
+
 docsearch({
   appId: "{{ .appId }}",
   apiKey: "{{ .apiKey }}",
+  container: '#docSearch',
+  debug: false, // Set debug to true if you want to inspect the modal
   indexName: "{{ .indexName }}",
   insights: true, // Optional, automatically send insights when user interacts with search results
-  container: '#docSearch',
-  debug: false // Set debug to true if you want to inspect the modal
+  transformSearchClient: (searchClient: any) => ({
+    ...searchClient,
+    search: debounce(searchClient.search, 200)
+  })
 });
 
 const onClick = function() {
