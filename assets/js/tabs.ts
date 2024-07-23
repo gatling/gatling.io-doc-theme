bcd92f8c94cc0875ff959b40d67215b4ffa1d333
Guillaume Corré <gui.corre@gmail.com>
2024-07-23 15:17:45 +0200

    feat: synchronize between similar code toggles by key
    Motivation:
    
    - Currently, only "languages" toggle are synchronized through the
      include-code shortcode
    - Most code-toggle are used for platform related commands but are not
      synchronized and always default to Linux/MacOS
    
    Modification:
    
    - Replace the formatting argument of code-toggle by a key that means
      "synchronize this code-toggle with others that share the same key"
    - If no key is specified, generated a unique id (per page) for this
      code-toggle to make it independant
    - If the key is was generated, it will be remembered to later page views
    - Choose a default value for the platform key that depends on the actual
      user platform

--- assets/js/tabs.ts
+++ assets/js/tabs.ts
@@ -3,6 +3,8 @@ const DATA_TOGGLE_VALUE = "data-toggle-value";
 
 const ACTIVE_CLASS = "active";
 
+const RANDOMIZED_DATA_PREFIX = "code-toggle-";
+
 const selectTabEvent = (targetKey: string, targetValue: string) => {
   document.querySelectorAll(`[${DATA_TOGGLE_KEY}='${targetKey}']`).forEach(selected => {
     selected.classList.remove(ACTIVE_CLASS);
@@ -10,25 +12,21 @@ const selectTabEvent = (targetKey: string, targetValue: string) => {
       selected.classList.add(ACTIVE_CLASS);
     }
   })
-}
+};
 
-const DEFAULTS_TABS = {
+const TABS_DEFAULTS = {
   "build-tool": "maven",
-  "language": "java"
-}
+  "language": "java",
+  "platform": window.navigator.platform.indexOf("Win") !== -1 ? "Windows" : "Linux/MacOS"
+};
 
 const toggleAllTabsOnClick = (targetKey: string, targetValue: string) => {
-  if (targetKey in DEFAULTS_TABS) {
-    // We store the config language selected in users' localStorage
-    localStorage.setItem(targetKey, targetValue);
-
-    if (targetKey === "language") {
-      if (window.gtag !== undefined && window.languages !== undefined && window.languages.hasOwnProperty(targetValue)) {
-        window.gtag("event", "Language toggle clicked", {
-          event_category: "Documentation DSL",
-          event_label: window.languages[targetValue],
-        });
-      }
+  if (targetKey === "language") {
+    if (window.gtag !== undefined && window.languages !== undefined && window.languages.hasOwnProperty(targetValue)) {
+      window.gtag("event", "Language toggle clicked", {
+        event_category: "Documentation DSL",
+        event_label: window.languages[targetValue],
+      });
     }
   }
 
@@ -38,19 +36,20 @@ const toggleAllTabsOnClick = (targetKey: string, targetValue: string) => {
 for (const tab of document.querySelectorAll(`button[${DATA_TOGGLE_KEY}]`)) {
   tab.addEventListener("click", (event) => {
     event.preventDefault();
-    toggleAllTabsOnClick(
-      event.currentTarget.getAttribute(DATA_TOGGLE_KEY),
-      event.currentTarget.getAttribute(DATA_TOGGLE_VALUE)
-    )
+
+    const targetKey = event.currentTarget.getAttribute(DATA_TOGGLE_KEY);
+    const targetValue = event.currentTarget.getAttribute(DATA_TOGGLE_VALUE);
+    toggleAllTabsOnClick(targetKey, targetValue);
+
+    if (!targetKey.startsWith(RANDOMIZED_DATA_PREFIX)) {
+      // We store the config selected in users' localStorage
+      localStorage.setItem(targetKey, targetValue);
+    }
   });
 }
 
-// Clean up old items
-if (localStorage.hasOwnProperty("configLangPref")) {
-  localStorage.removeItem("configLangPref");
-}
-
-for (const [key, defaultValue] of Object.entries(DEFAULTS_TABS)) {
+for (const [key, defaultValue] of Object.entries(TABS_DEFAULTS)) {
   const value = localStorage.getItem(key) ?? defaultValue;
+
   selectTabEvent(key, value);
 }
